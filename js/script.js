7d35e8807d81ca8337e4acec447149ade1ac87ea
BRITVA-hp <borbritvahp@gmail.com>
2022-12-16 14:37:48 +0400

    ready

--- js/script.js
+++ js/script.js
@@ -13,4 +13,19 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.classList.remove('menu--active')
     ])
 
+    //bar
+    const barClose = document.querySelector('.bar__close')
+    const bar = document.querySelector('.bar')
+    const barOpen = document.querySelector('.bar__open')
+
+    barClose.addEventListener('click', () => {
+        bar.classList.remove('bar--active')
+        barOpen.classList.add('bar__open--active')
+    })
+
+    barOpen.addEventListener('click', () => {
+        bar.classList.add('bar--active')
+        barOpen.classList.remove('bar__open--active')
+    })
+
 })
